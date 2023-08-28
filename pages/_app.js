c432a5904cee47a28fed4c021abbff6079716e75
rodcasanova12 <60192482+rodcasanova12@users.noreply.github.com>
2023-08-28 13:43:11 -0400

    Ecommerce Store

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
+
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
+}
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'lh53is34',
+    dataset: 'production'
+  }
+})
