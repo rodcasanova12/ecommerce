d378b7696f694672bae87ab42370985f04cacbd3
rodcasanova12 <60192482+rodcasanova12@users.noreply.github.com>
2023-08-28 23:08:26 -0400

    buy now implemented

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -9,6 +9,7 @@ const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  
 
   const handleBuyNow = () => {
     onAdd(product, qty);
@@ -111,6 +112,7 @@ export const getStaticProps = async ({ params: { slug }}) => {
   const products = await client.fetch(productsQuery);
 
   console.log(product);
+  
 
   return {
     props: { products, product }
